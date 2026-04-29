25ece99e627e4d80314c6d360abd34747d9d6700
BusterAlan <1120030@alumno.um.edu.mx>
2026-04-29 17:41:02 -0600

    Add package_info_plus & macOS plugin
    Add package_info_plus to pubspec and register its macOS plugin in GeneratedPluginRegistrant so the app can access package metadata on macOS. Also remove an unused auto_route import from submit_demonlist_fab.dart to tidy imports.

--- lib/features/shared/presentation/widgets/submit_demonlist_fab.dart
+++ lib/features/shared/presentation/widgets/submit_demonlist_fab.dart
@@ -1,10 +1,7 @@
-import "package:auto_route/auto_route.dart";
 import "package:dotted_border/dotted_border.dart";
 import "package:flutter/material.dart";
 import "package:flutter_common_classes/extensions/theme_extension.dart";
 import "package:font_awesome_flutter/font_awesome_flutter.dart";
-
-import "../../../../core/routes/app_router.gr.dart";
 
 /// Submit demonlist custom floating action button to implement in whatever page needs
 class SubmitDemonlistFAB extends StatelessWidget {
